ad71276344f69eb99b88e4b002a95bda40ed4c0f
Mo <mohman556@gmail.com>
2021-09-16 22:16:16 -0400

    Changes to main function

---
+++ src/AddressBook.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class AddressBook {
+    public ArrayList<BuddyInfo> contactList;
+
+    public AddressBook(){
+        contactList = new ArrayList<BuddyInfo>();
+    }
+
+    public static void main(String[] args){
+        System.out.println("Address Book: ");
+        BuddyInfo Buddy = new BuddyInfo("Homer", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(Buddy);
+        addressBook.removeBuddy(Buddy);
+    }
+
+    public void addBuddy(BuddyInfo Buddy){
+        if (Buddy != null){
+            contactList.add(Buddy);
+            System.out.println("Added " + Buddy.getName() + " To Address book");
+        }
+    }
+
+    public void removeBuddy(BuddyInfo Buddy){
+        if (!contactList.isEmpty()) {
+            for (Iterator<BuddyInfo> iter = contactList.iterator(); iter.hasNext();) {
+                BuddyInfo buddy = iter.next();
+                if (buddy.equals(Buddy)) {
+                    iter.remove();
+                    System.out.println("Removed " + Buddy.getName() + " From Address Book");
+                }
+            }
+        }
+    }
+
+}
